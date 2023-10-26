5e38a566518c5b722752113da51ce3b40e822c13
Eman Gurung <egurung1117@gmail.com>
2023-10-26 04:11:56 -0400

    added foreign keys to Like

--- server/models.py
+++ server/models.py
@@ -41,4 +41,7 @@ class Like(db.Model, SerializerMixin):
     __tablename__ = "likes"
 
     id = db.Column(db.Integer, primary_key = True)
+
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     
