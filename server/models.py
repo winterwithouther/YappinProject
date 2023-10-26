9a991332d31c482c6525680d6aecac6c4f149114
Eman Gurung <egurung1117@gmail.com>
2023-10-26 04:01:08 -0400

    added Post Model

--- server/models.py
+++ server/models.py
@@ -3,4 +3,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Post(db.Model, SerializerMixin):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String)
+    caption = db.Column(db.String)
