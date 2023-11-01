92ce8a465167e7db3cd3ed484eb8e1beb6a57270
Eman Gurung <egurung1117@gmail.com>
2023-11-01 13:55:25 -0400

    creating, editing, deleting comments done

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -1,8 +1,65 @@
-import React from "react"
+import React, { useState } from "react"
 
-function CommentCard({content, user}) {
+function CommentCard({content, id, user, commentUser, commentRemoved, commentEdited}) {
+    const [editComment, setEditComment] = useState(false)
+    const [comment, setComment] = useState(content)
+
+    function handleDeleteComment() {
+        fetch(`/comments/${id}`, {
+            method : "DELETE"
+        })
+        .then(response => response.json())
+        .then(() => {
+            commentRemoved(id)
+        })
+    }
+
+    function handleEditComment() {
+        setEditComment(true)
+    }
+
+    function handleChangeEditComment(e) {
+        setComment(e.target.value)
+        console.log(comment)
+    }
+
+    function handleSubmitEditComment(e) {
+        e.preventDefault()
+
+        fetch(`/comments/${id}`, {
+            method : "PATCH",
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify({content : comment})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            commentEdited(id, data)
+            setEditComment(false)
+        })
+    }
+
+    function handleCancelEdit() {
+        setEditComment(false)
+    }
+
+    if (editComment) {
+        return <div>
+            <form onSubmit={handleSubmitEditComment}>
+                <input type="text" name="comment" value={comment} onChange={handleChangeEditComment}></input>
+                <input type="submit" value="submit"></input>
+                <button onClick={handleCancelEdit}>Cancel</button>
+            </form>
+        </div>
+    }
+
     return <div>
-        <h5>{user.username} : {content}</h5>
+        <h5>{commentUser.username} : {content}</h5>
+        {user.id == commentUser.id ? 
+        <div>
+        <button onClick={handleEditComment}>Edit</button>
+        <button onClick={handleDeleteComment}>Delete</button>
+        </div> : ""}
     </div>
 }
 
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -6,12 +6,37 @@ function PostCard({caption, content, comments, user, id, likes}) {
     const [likeObj, setLikeObj] = useState({})
     const [totalLikes, setTotalLikes] = useState(0)
     const [comments2, setComments] = useState([])
+    const [commenting, setCommenting] = useState(false)
+    const [createComment, setCreateComment] = useState("")
+
+    function commentRemoved(id) {
+        const newComments = comments2.filter((comment) => {
+            return comment.id !== id
+        })
+        setComments(newComments)
+    }
+
+    function commentEdited(id, data) {
+        const newComments = comments2.map(comment => {
+            if (comment.id === id) {
+                return data
+            } else {
+                return comment
+            }
+        })
+
+        setComments(newComments)
+    }
 
     const commentsDisplay = comments2.map(comment => {
         return <CommentCard
                 key={comment.id}
                 content={comment.content}
-                user={comment.user}
+                commentUser={comment.user}
+                user={user}
+                id={comment.id}
+                commentRemoved={commentRemoved}
+                commentEdited={commentEdited}
                 />
     })
 
@@ -53,11 +78,44 @@ function PostCard({caption, content, comments, user, id, likes}) {
         }
     }
 
+    function handleChangeCreateComment(e) {
+        setCreateComment(e.target.value)
+    }
+
+    function handleSubmitCreateComment(e) {
+        e.preventDefault()
+        fetch("/comments", {
+            method : "POST",
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify({content : createComment, post_id : id, user_id : user.id})
+        })
+        .then(response => response.json())
+        .then(data => {
+            setComments([...comments2, data])
+            setCommenting(false)
+            console.log(comments2)
+        })
+    }
+
+    function handleClickCancel() {
+        setCommenting(false)
+    }
+
+    function handleClickComment() {
+        setCommenting(true)
+    }
+
     return <div>
         <img src={content}></img>
         <h2>Likes : {totalLikes}</h2>
         <button onClick={handleLike}>{like ? "Unlike" : "Like"}</button>
         <h4>USERNAME : {caption}</h4>
+        {commenting ? 
+        <form onSubmit={handleSubmitCreateComment}>
+            <input name="create-comment" type="text" value={createComment} onChange={handleChangeCreateComment}></input>
+            <input type="submit" value="comment"></input>
+            <button onClick={handleClickCancel}>cancel</button>
+        </form> : <button onClick={handleClickComment}>Comment</button>}
         <div>
             <h4>Comments:</h4>
             {commentsDisplay}
